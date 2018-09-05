729a29fc91776b04d79df762629ef36d43103560
Unknown <b1845888@planet.kanazawa-it.ac.jp>
2018-09-05 14:35:04 +0900

    炙り出しと壺（持って投げるとカギが出てくる）プログラムです

---
+++ 壺と炙り出しのスクリプト/Aburidasi.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Aburidasi : MonoBehaviour {
+    float alfa;
+    public float speed  = 0.01f;
+    private float timeCount = 3;//時間の制限
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+
+        alfa = 0;　　 //透明度
+        time = 0;     //待機時間
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Effect_Eria")){ //ろうそくの火の周り(Effect_Eriaのタブを持った範囲)に当たった時
+
+            time += 0.015f;
+
+            if (timeCount <= time) //約三秒待ったとき
+            {
+                GetComponent<Renderer>().material.color = new Color(255, 0, 0, alfa);　//透明度の変化
+
+                alfa += speed;　//透明度の変化
+
+            }
+
+        };
+
+       
+    }
+		
+	
+}
---
+++ 壺と炙り出しのスクリプト/Break_tubo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break_tubo : MonoBehaviour {
+    public GameObject obj;　
+
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("ground")){　//「グラウンド」タグを取得したとき
+
+            Destroy(gameObject); //壺を破壊
+
+            GameObject instance = (GameObject)Instantiate(obj);　//プレハブから出現させる
+
+            instance.transform.position = gameObject.transform.position;　//衝突した座標を取得してプレハブを設置
+
+        };
+
+
+    }
+
+}
