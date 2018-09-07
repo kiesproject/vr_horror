96c1bbcd290411e59afdf1c61f7e922c38ad120a
Ryuichi Komatsu <vrmmorpg202200@gmail.com>
2018-09-07 13:51:49 +0900

    懐中電灯のスイッチ

---
+++ swith.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swith : MonoBehaviour
+{
+    public GameObject KLight; //オブジェクト名
+    Light light;
+    public bool LightState = false; //ライトの初期設定
+
+    // Use this for initialization
+    void Start()
+    {
+        light = KLight.GetComponent<Light>();
+        light.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //マウス左クリック時反転
+        {
+            OnOff();
+            if(LightState == false)
+            {
+                LightState = true;
+            }
+            else
+            {
+                LightState = false;
+            }
+        }
+    }
+
+    void OnOff()
+    {
+        if (LightState == false)
+        {
+            light.intensity = 1;
+        }
+        else
+        {
+            light.intensity = 0;
+        }
+    }
+}
