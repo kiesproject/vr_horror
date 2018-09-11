ae3efb7dea219be36430506d2dc522c1f2b4713c
Unknown <b1849734@planet.kanazawa-it.ac.jp>
2018-09-11 13:30:04 +0900

    ドアのスクリプト

---
+++ DoorR.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorR : MonoBehaviour {
+	private Animator anim;
+
+	// Use this for initialization
+	void Start()
+	{
+		anim = transform.parent.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		if (col.gameObject.tag == "Player" & count == 0) {
+			anim.SetTrigger ("Open");
+		}
+	}
+
+
+}
