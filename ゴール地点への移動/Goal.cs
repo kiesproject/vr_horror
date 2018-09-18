7782e680f387370b5d6b2710f7e82884188f93d5
Unknown <b1845888@planet.kanazawa-it.ac.jp>
2018-09-18 11:25:18 +0900

    ゴール地点へ移動するスクリプト

---
+++ ゴール地点への移動/Goal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+    public float count;
+    public float alfa;
+    public float alfa2;
+    public float time;
+    public Text text;
+
+    // Use this for initialization
+    void Start () { 
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alfa += 0.015f;
+        text.color = new Color(255, 0, 0, alfa);　//透明度の変化
+
+        if (alfa >= 5)
+        {
+            
+            alfa2 += 0.015f;
+            text.text = "Thank you for playing"; //テキストの変更
+            text.color = new Color(255, 0, 0, alfa2);//透明度の変化
+
+            count += 0.01f;
+
+            if (count >= 4)//約５秒
+            {
+                
+                SceneManager.LoadScene("Horror_Poject");//スタート画面のシーンに移動
+
+            }
+        }
+    }
+}
---
+++ ゴール地点への移動/Tereport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tereport : MonoBehaviour {
+    public GameObject pla;　//プレイヤー
+
+	// Use this for initialization
+	void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Sciene2");//ゴール地点のシーンに移動
+
+        }
+    }
+}
