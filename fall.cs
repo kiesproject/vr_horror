65b5aeda8ff8778967bab0de98f16261052cf4e5
nezu arisa <b1819296@planet.kanazawa-it.ac.jp>
2018-09-21 18:11:46 +0900

    掛け軸が落ちる

---
+++ fall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fall : MonoBehaviour {
+    public Transform hand;
+    public Transform kakeziku;
+    private Rigidbody rigid;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+        rigid.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Vector3.Distance(hand.position, kakeziku.position) >= 5)
+        {
+            rigid.isKinematic = false;
+        }
+		
+	}
+}
