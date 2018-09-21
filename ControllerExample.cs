98b3b947f92dfb343f13a13568213f6b002b9cc9
Unknown <b1849734@planet.kanazawa-it.ac.jp>
2018-09-21 18:00:04 +0900

    スタート画面からのシーン移動

---
+++ ControllerExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllerExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			SceneManager.LoadScene ("main");
+		}
+
+	}
+}
