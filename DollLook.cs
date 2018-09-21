79381c8e303ac788815371ca1321ede013abb5c4
nezu arisa <b1819296@planet.kanazawa-it.ac.jp>
2018-09-21 17:23:14 +0900

    人形の首と仏壇が動く

---
+++ DollLook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollLook : MonoBehaviour {
+    public Transform player;
+    public static bool Turn;
+    bool T = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Turn == true)
+        {
+            if (T == false) {
+                this.gameObject.transform.LookAt(player);
+                T = true;
+            } }
+		
+	}
+}
---
+++ DollMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollMove : MonoBehaviour {
+    public Transform player;
+    public GameObject DollTurn;
+    public GameObject Butsudan;
+    public GameObject hand;
+    bool A = false;
+
+	// Use this for initialization
+	void Start () {
+        Butsudan = GameObject.Find("Butsudan");
+        hand = GameObject.Find("Hand");
+        DollTurn = GameObject.Find("DollTurn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var trackedObject = GetComponent<SteamVR_TrackedObject>();
+        //var device = SteamVR_Controller.Input((int)trackedObject.index);
+        //if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if ((Vector3.Distance(this.gameObject.transform.position, hand.gameObject.transform.position) <= 5.0f)) {
+                if (A == false) {
+                    this.gameObject.transform.LookAt(player);
+                    Butsudan.gameObject.transform.Translate(0, 5, 0);
+                    A = true;
+                    DollLook.Turn = true;
+                } }
+        }
+
+    }
+}
