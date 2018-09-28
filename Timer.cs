95a87bb89d301191ac11177fcbfeb4ea966faf69
Unknown <b1845811@planet.kanazawa-it.ac.jp>
2018-09-28 17:34:54 +0900

    タイマーのスクリプトです。

---
+++ Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    float time;
+    TextMesh text;
+
+    void Start()
+    {
+        time = 0;
+        text = GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        text.text = Mathf.Floor(time).ToString();
+    }
+}
