7ac9c7508d30ba8136e52e282c44e04b74279514
Unknown <b1845888@planet.kanazawa-it.ac.jp>
2018-09-28 18:29:33 +0900

    タッチパッド押しっぱなしで移動するスクリプト

---
+++ Player_Controler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controler : MonoBehaviour {
+    public Rigidbody Player;
+    public Transform main_camera;
+    public GameObject LC; //VRのコントローラー
+
+	// Use this for initialization
+	void Start () {
+
+        Player = GetComponent<Rigidbody>();
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+        var trackedObject = LC.GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        Vector3 z = main_camera.transform.forward;//カメラの青軸を取得
+        
+        if(device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Player.velocity = z;//プレイヤーがカメラの向いた方向に移動
+        }
+		
+	}
+
+}
