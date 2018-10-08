2e77aff8d49248403ccf72dfcc7ca9e11921536a
Ryuichi Komatsu <vrmmorpg202200@gmail.com>
2018-10-08 09:26:34 +0900

    GameOver 切り替え

---
+++ change.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class change : MonoBehaviour
+{
+    float countTime = 5; //タイマーの時間
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime -= Time.deltaTime; //スタートしてからの秒数を引く
+
+        if (countTime <= 0.01)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
